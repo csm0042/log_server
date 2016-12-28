f95d49404de81349e17c91e879b38b5deff8c861
csmaue@gmail.com <thpbth5150>
2016-12-27 19:45:28 -0600

    Works

--- log_server/main.py
+++ log_server/main.py
@@ -5,13 +5,13 @@
 # Import Required Libraries (Standard, Third Party, Local) ****************************************
 import datetime
 import logging
+import logging.handlers
 import os
 import sys
 import time
 import multiprocessing
 from multiprocessing.connection import Listener
 sys.path.insert(0, os.path.abspath('..'))
-import logger_mp
 import message
 
 
@@ -26,19 +26,6 @@ __email__ = "csmaue@gmail.com"
 __status__ = "Development"
 
 
-# Incoming Message Handler Sub-Process ************************************************************
-def connection_listener(host, port, password, msg_in_queue):
-    """ separate process to handle incoming connections """
-    address = (host, port)
-    listener = Listener(address, authkey=password)
-
-    while True:
-        conn = listener.accept()
-        msg = conn.recv()
-        conn.close()
-        msg_in_queue.put_nowait(msg)
-
-
 # Main process class ******************************************************************************
 class LogServer(object):
     """ Log server that listens on port 6000 for incoming messages from other processes """
@@ -46,100 +33,75 @@ class LogServer(object):
         # Set up local logging
         self.debug_logfile = (os.path.dirname(os.path.abspath(__file__)) + "/logs/debug.log")
         self.info_logfile = (os.path.dirname(os.path.abspath(__file__)) + "/logs/info.log")
-        logger_mp.listener_configurer(self.debug_logfile, self.info_logfile)
+        self.setup_log_handlers(self.debug_logfile, self.info_logfile)
         self.logger = logging.getLogger(__name__)
         self.master_logger = None
-        # Define control tags
-        self.close_pending = False
-        self.in_msg_loop = True
-        self.shutdown_time = None
         self.heartbeat = datetime.datetime.now()
-        # Define various queues used for sorting and processing of messages
-        self.msg_in_queue = multiprocessing.Queue(-1)
-        self.log_queue = multiprocessing.Queue(-1)
-        self.work_queue = multiprocessing.Queue(-1)
-        self.msg_out_queue = multiprocessing.Queue(-1)
-        self.msg = None
-        self.msg_in = None
-        self.log_record = None
-        self.work_record = None
-        # Define sub-processed which will listen for incoming messages
-        self.msg_in_proc = multiprocessing.Process(
-            target=connection_listener, args=("localhost", 6000, b"password", self.msg_in_queue))
+        self.shutdown_time = None
+        self.main_loop = True
+        self.setup_listener_connection("localhost", 6000, b"password")
+        self.conn = None
 
 
+    def setup_log_handlers(self, debug_logfile, info_logfile):
+        root = logging.getLogger()
+        root.handlers = []
+        # Create desired handlers
+        debug_handler = logging.handlers.TimedRotatingFileHandler(debug_logfile, when="h", interval=1, backupCount=24, encoding=None, delay=False, utc=False, atTime=None)
+        info_handler = logging.handlers.TimedRotatingFileHandler(info_logfile, when="h", interval=1, backupCount=24, encoding=None, delay=False, utc=False, atTime=None)
+        console_handler = logging.StreamHandler()
+        # Create individual formats for each handler
+        debug_formatter = logging.Formatter('%(processName)-16s,  %(asctime)-24s,  %(levelname)-8s, %(message)s')
+        info_formatter = logging.Formatter('%(processName)-16s,  %(asctime)-24s,  %(levelname)-8s, %(message)s')    
+        console_formatter = logging.Formatter('%(processName)-16s,  %(asctime)-24s,  %(levelname)-8s, %(message)s')
+        # Set formatting options for each handler
+        debug_handler.setFormatter(debug_formatter)
+        info_handler.setFormatter(info_formatter)
+        console_handler.setFormatter(console_formatter)
+        # Set logging levels for each handler
+        debug_handler.setLevel(logging.DEBUG)
+        info_handler.setLevel(logging.INFO)
+        console_handler.setLevel(logging.INFO)
+        # Add handlers to root logger
+        root.addHandler(debug_handler)
+        root.addHandler(info_handler)
+        root.addHandler(console_handler)
+        
 
-    def process_in_buffer(self):
-        """ This method cycles through the incoming communication buffer and sorts the messages out
-        by type """
-        while True:
-            try:
-                self.msg_in = self.msg_in_queue.get_nowait()
-                if isinstance(self.msg_in, message.Message):
-                    self.work_queue.put_nowait(self.msg_in)
-                    self.msg_in = None
-                elif isinstance(self.msg_in, logging.LogRecord):
-                    self.log_queue.put_nowait(self.msg_in)
-                    self.msg_in = None
-                else:
-                    self.msg_in = None
-            except:
-                break
-
-
-    def process_log_buffer(self):
-        """ Processes log messages received from other processes via whatever handlers are
-        currenntly defined. """
-        while True:
-            try:
-                self.log_record = self.log_queue.get_nowait()
-                if isinstance(self.log_record, logging.LogRecord) is True:
-                    self.master_logger = logging.getLogger(self.log_record.name)
-                    self.master_logger.handle(self.log_record)
-                    self.log_record = None
-                else:
-                    self.log_record = None
-            except:
-                break
+    def setup_listener_connection(self, host, port, password):
+        """ Set up a listener object """
+        self.listener = Listener((host, port), authkey=password)
 
 
-    def process_work_buffer(self):
-        """ Processes any non-alarm messages received from other processes """
-        while True:
-            try:
-                self.work_record = None
-                self.work_record = self.msg_in_queue.get_nowait()
-                if self.work_record.dest == 6000:
-                    # If message is a heartbeat, update heartbeat and reset
-                    if self.work_record.type == "001":
-                        self.heartbeat = datetime.datetime.now()
-                    elif self.work_record.type == "999":
-                        self.logger.info("Kill code received - Shutting down")
-                        self.shutdown_time = datetime.datetime.now()
-                        self.close_pending = True
-                else:
-                    self.msg_out_queue.put_nowait(self.work_record)
-            except:
-                break
+    def process_log_record(self, record):
+        self.master_logger = logging.getLogger(record.name)
+        self.master_logger.handle(record)
+
+
+    def process_message(self, msg):
+        if msg.dest == 6000:
+            # If message is a heartbeat, update heartbeat and reset
+            if msg.type == "001":
+                self.heartbeat = datetime.datetime.now()
+                print("Resetting heartbeat")
+            elif msg.type == "999":
+                self.logger.info("Kill code received - Shutting down")
+                self.shutdown_time = datetime.datetime.now()
+                self.main_loop = False
 
 
     def run(self):
-        self.logger.info("Log Server Started")
-        self.msg_in_proc.start()
-        while True:
-            self.process_in_buffer()
-            self.process_log_buffer()
-            self.process_work_buffer()
-            time.sleep(0.011)
-
+        """ Runs a connection listener and processes any messages that are received """
+        while self.main_loop is True:
+            self.conn = self.listener.accept()
+            self.msg = self.conn.recv()
+            self.conn.close()
+            if isinstance(self.msg, logging.LogRecord):
+                self.process_log_record(self.msg)
+            elif isinstance(self.msg, message.Message):
+                self.process_message(self.msg)
 
 
 if __name__ == "__main__":
-    this_process = LogServer()
-    this_process.run()
-
-
-
-
-
-
+    listener = LogServer()
+    listener.run()
